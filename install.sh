2c4f914576e0512ad5adcc6f0d68c55716332044
Nanashi <johnnyjel1234567890@gmail.com>
2024-02-28 00:21:33 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ git clone https://github.com/NanashiTheNameless/BalatroModLoader.git $temp_dir_g
 
 balatro_dir="$(locate -e common/Balatro | grep '/Balatro$' | head -n 1)"
 
-balatro_path="$balatrodir/Balatro.exe"
+balatro_path="$balatro_dir/Balatro.exe"
 
 7zz x "$balatro_path" -o"$temp_dir"
 
@@ -16,16 +16,18 @@ echo "Extraction completed to $temp_dir."
 sed -i '/self\.SPEEDFACTOR = 1/a \    initSteamodded()' $temp_dir/game.lua
 
 echo "" > "$temp_dir_git/staging.lua"
-cat "$temp_dir_git/BalatroModLoader/Lua/core.lua" >> "$temp_dir_git/staging.lua"
+cat "$temp_dir_git/lua/core.lua" >> "$temp_dir_git/staging.lua"
 echo "" >> "$temp_dir_git/staging.lua"
-cat "$temp_dir_git/BalatroModLoader/Lua/debug.lua" >> "$temp_dir_git/staging.lua"
+cat "$temp_dir_git/lua/debug.lua" >> "$temp_dir_git/staging.lua"
 echo "" >> "$temp_dir_git/staging.lua"
-cat "$temp_dir_git/BalatroModLoader/Lua/loader.lua" >> "$temp_dir_git/staging.lua"
+cat "$temp_dir_git/lua/loader.lua" >> "$temp_dir_git/staging.lua"
 echo "" >> "$temp_dir_git/staging.lua"
-cat "$temp_dir_git/BalatroModLoader/Lua/deck.lua" >> "$temp_dir_git/staging.lua"
+cat "$temp_dir_git/lua/deck.lua" >> "$temp_dir_git/staging.lua"
 echo "" >> "$temp_dir_git/staging.lua"
-cat "$temp_dir_git/BalatroModLoader/Lua/joker.lua" >> "$temp_dir_git/staging.lua"
+cat "$temp_dir_git/lua/joker.lua" >> "$temp_dir_git/staging.lua"
 
-7zz a "$balatro_path" "$temp_dir_git/BalatroModLoader/Lua/main.lua"
+cat "$temp_dir_git/staging.lua" >> "$temp_dir/main.lua"
 
-\rm -r $temp_dir $temp_dir_git
+7zz a "$balatro_path" "$temp_dir/main.lua"
+
+rm -rf $temp_dir $temp_dir_git
